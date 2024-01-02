535cf0f357d6bb364a9788be101e3bf2052b1207
SelvaFront <selva@caliberfocus.com>
2024-01-02 15:59:37 +0530

    View only mode Intergrated

--- Frontend/src/app/pages/procedure/procedure-work-area/procedure-details/procedure-details.component.ts
+++ Frontend/src/app/pages/procedure/procedure-work-area/procedure-details/procedure-details.component.ts
@@ -10,15 +10,25 @@ export class ProcedureDetailsComponent implements OnInit{
 
   @Input() StageValue:any;
   mainTabsValue:any;
+  header_viewOnlymode:any[] = [];
+  isFirstOpen:boolean = true;
   constructor(private http : HttpClient){}
 
-  ngOnInit(): void {
+  ngOnInit() {
     this.http.get('assets/json/main-tabs.json').subscribe((res:any)=>{
       this.mainTabsValue = res;
+    });
+
+    this.http.get('assets/json/viewOnlyMode.json').subscribe((res:any)=>{
+      console.log('a',res);
+      this.header_viewOnlymode = res;
     })
   }
 
   CloseViewOnlyMode(){
     document.body.classList.toggle('selva');
+  }
+  open(){
+    window.alert('Hi You ahve opened the testing modal')
   }
 }
