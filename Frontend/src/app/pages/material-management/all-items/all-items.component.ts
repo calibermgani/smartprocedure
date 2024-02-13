1f8b0524f792659fd7ce8ba2080d67ea879013a3
selvaFront <selva@caliberfocus.com>
2024-02-13 11:52:01 +0530

    changes in add item

--- Frontend/src/app/pages/material-management/all-items/all-items.component.ts
+++ Frontend/src/app/pages/material-management/all-items/all-items.component.ts
@@ -55,6 +55,16 @@ export class AllItemsComponent implements OnInit {
   @ViewChild('viewitem', { static: false }) viewitem?: ModalDirective;
   @ViewChild('notify', { static: false }) notify?: ModalDirective;
 
+  cartData: any[];
+  subtotal: any = 0;
+  discount: any;
+  discountRate = 0.15;
+  shipping: any;
+  shippingRate: any = '65.00';
+  tax: any;
+  taxRate = 0.125;
+  totalprice: any;
+
   options: Options = {
     floor: 0,
     ceil: 250
@@ -117,11 +127,6 @@ export class AllItemsComponent implements OnInit {
       this.folder_structure_value = res;
       console.log('response',this.folder_structure_value);
     });
-
-    setTimeout(() => {
-      this.additem?.show()
-    }, 1000);
-
   }
 
 
@@ -480,5 +485,27 @@ export class AllItemsComponent implements OnInit {
   resize:boolean = false;
   hideAdvancedFilters(){
     this.resize =! this.resize;
+  }
+
+  qty: number = 0;
+  calculateQty(id: any, qty: any, i: any) {
+    this.subtotal = 0;
+    if (id == '0' && qty > 1) {
+      qty--;
+      this.cartData[i].qty = qty
+      this.cartData[i].total = (this.cartData[i].qty * this.cartData[i].price).toFixed(2)
+    }
+    if (id == '1') {
+      qty++;
+      this.cartData[i].qty = qty
+      this.cartData[i].total = (this.cartData[i].qty * this.cartData[i].price).toFixed(2)
+    }
+    this.cartData.map((x: any) => {
+      this.subtotal += parseFloat(x['total'])
+    })
+    this.subtotal = this.subtotal.toFixed(2)
+    this.discount = (this.subtotal * this.discountRate).toFixed(2)
+    this.tax = (this.subtotal * this.taxRate).toFixed(2);
+    this.totalprice = (parseFloat(this.subtotal) + parseFloat(this.tax) + parseFloat(this.shippingRate) - parseFloat(this.discount)).toFixed(2)
   }
 }
